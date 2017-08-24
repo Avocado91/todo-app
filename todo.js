7642c6c1cdb433db91776c10f2964fbdda2ab7e2
T.J <t.ruggles@live.ca>
2017-08-24 03:32:32 -0400

    adding delete logic for todos

--- todo.js
+++ todo.js
@@ -2,3 +2,11 @@
 $('li').click(function() {
   $(this).toggleClass('completed');
 });
+
+//delete todos with fadeOut
+$('span').click(function(event) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  })
+  event.stopPropagation();
+});
